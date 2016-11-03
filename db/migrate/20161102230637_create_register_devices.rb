f151d16dab17e85d7e3fccdf581f359620532a44
Hai Le T <tanhai.infotech@gmail.com>
2016-11-03 07:05:18 +0700

    create models

--- db/migrate/20161102230637_create_register_devices.rb
+++ db/migrate/20161102230637_create_register_devices.rb
@@ -3,6 +3,8 @@ class CreateRegisterDevices < ActiveRecord::Migration[5.0]
     create_table :register_devices do |t|
       t.string :status
       t.text :reason
+      t.string :from
+      t.string :to
       t.references :user, foreign_key: true
       t.references :device, foreign_key: true
 
