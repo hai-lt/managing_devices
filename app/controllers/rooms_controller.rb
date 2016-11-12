2d3255edcada92358cf0b7e26916f1da4b143ff2
Hai Le T <tanhai.infotech@gmail.com>
2016-11-12 07:06:24 +0700

    create some controllers

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,22 @@
+class RoomsController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
--- db/seed/device.rb
+++ db/seed/device.rb
@@ -1,3 +1,4 @@
 brands = %w[Dell Acer HP Tosiba Apple]
 category_ids = Category.select(:id)
 rooms = Room.select(:id)
+status = %w[Normal Good Bad]
