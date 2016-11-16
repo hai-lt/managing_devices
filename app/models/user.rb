ac2a9f3a642803a97fe67907fb4c790bc01fd4e4
Hai Le T <tanhai.infotech@gmail.com>
2016-11-16 22:52:20 +0700

    add bscryp gem to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
   belongs_to :position
   has_many :register_devices
   has_many :registers, through: :register_devices, foreign_key: :user_id, source: :device
   has_many :devices, -> { where(positions: { position: 'md' }) }
   has_many :report_devices
   has_many :reports, through: :report_devices, foreign_key: :device_id, source: :device
+
+  validates :password_confirmation, presence: { message: 'User: password_confirmation can not be blank' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102231522) do
+ActiveRecord::Schema.define(version: 20161116153535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,13 +95,13 @@ ActiveRecord::Schema.define(version: 20161102231522) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password"
     t.string   "address"
     t.string   "phone"
     t.string   "avatar"
     t.integer  "position_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
     t.index ["position_id"], name: "index_users_on_position_id", using: :btree
   end
 
