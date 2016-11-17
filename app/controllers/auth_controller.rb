1c20668115a75d01cfc35054a7dbfa33558687d7
Hai Le T <tanhai.infotech@gmail.com>
2016-11-17 08:36:53 +0700

    define login and signup

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,15 +5,23 @@ class AuthController < ApplicationController
     access_token = generate_access_token(@user)
     AccessToken.create!(access_token: access_token, user: @user)
     if permit_params[:remember_me]
-      session
+      cookies[:access_token] = {
+        key: access_token,
+        expired: Time.now.next_month
+      }
     else
-
+      #save in session
     end
-
+    render '/'  #home page
   end
 
   def signup
-    
+    @user = User.new(signup_permit)
+    if @user.save
+      render '/' #home page
+    else
+      raise errors
+    end
   end
 
   private
@@ -21,11 +29,15 @@ class AuthController < ApplicationController
       params.permit(:email, :password, :remember_me)
     end
 
+    def signup_permit
+      params.permit(:email, :password, :password_confirmation)
+    end
+
     def generate_access_token(user)
       Digest::MD5::hexdigest(user.email.downcase + Time.new.to_i.to_s)
     end
 
     def current_user
-      @current_user ||= User.find_by(cookies[:access_token])
+      @current_user ||= AccessToken.find_by(access_token: cookies[:access_token]).try(:user)
     end
 end
