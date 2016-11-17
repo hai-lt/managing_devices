1faac61d121d0b1d8ef39780566a886129d5b345
Hai Le T <tanhai.infotech@gmail.com>
2016-11-17 07:33:02 +0700

    modify auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,7 +1,31 @@
 class AuthController < ApplicationController
   def login
+    @user = User.find_by(permit_params[:email])
+    raise errors unless @user.authenticate(permit_params[:password])
+    access_token = generate_access_token(@user)
+    AccessToken.create!(access_token: access_token, user: @user)
+    if permit_params[:remember_me]
+      session
+    else
+
+    end
+
   end
 
   def signup
+    
   end
+
+  private
+    def permit_params
+      params.permit(:email, :password, :remember_me)
+    end
+
+    def generate_access_token(user)
+      Digest::MD5::hexdigest(user.email.downcase + Time.new.to_i.to_s)
+    end
+
+    def current_user
+      @current_user ||= User.find_by(cookies[:access_token])
+    end
 end
