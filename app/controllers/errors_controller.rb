34e24ccd8d006ef51f4903cd85e1de9d2317c80c
Hai Le T <tanhai.infotech@gmail.com>
2016-12-04 21:25:17 +0700

    add site 403 error

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,5 @@
+class ErrorsController < ApplicationController
+  def permission_denied
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   resources :devices
 
   get 'systemconfig', to: 'systemconfig#system'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'permission_denied', to: 'errors#permission_denied'
 end
