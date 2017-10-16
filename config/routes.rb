ed40404910eb020ef2863483b35a7070b9e46fab
Hai Le T <tanhai.infotech@gmail.com>
2017-10-16 19:02:42 +0700

    [Add] cameras crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
+  resources :cameras
   post 'auth', to: 'auth#login'
   get 'auth', to: 'auth#create'
   get 'logout', to: 'auth#logout'
-  resources :timesheets
-  resources :light_plans
-  resources :users
 
   get 'permission_denied', to: 'errors#permission_denied'
 
-  get 'ard', to: 'ard#index'
-  get 'vihicle', to: 'ard#display'
-  root to: 'ard#display'
+  root to: 'cameras#index'
 end
